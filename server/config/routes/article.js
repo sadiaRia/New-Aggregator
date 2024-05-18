277ba990b45bf29a7b904eec0021d7ddfd292aa4
sadia <sadia.ria4@gmail.com>
2024-05-18 12:34:46 +0600

    functioanl all

--- server/config/routes/article.js
+++ server/config/routes/article.js
@@ -1,6 +1,8 @@
 const Article = require("../../article");
 const utils = require("../../utils");
 
+module.exports = (app) => {
 
-app.get('/article/search', utils.wrapAsync(Article.filterArticles));
+    app.get('/article/search', utils.wrapAsync(Article.filterArticles));
 
+}
